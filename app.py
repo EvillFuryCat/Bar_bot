8a1134a888c1541dc175bf6daf681df570fc6b64
Konstantin <k1st.volkov@gmail.com>
2023-03-15 19:09:33 +0300

    add feature class requests and button sale_products

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from aiogram import Bot, Dispatcher, executor, types
 
 from db import db_requests
-from database_queries import *
+from database_queries import BotDB
 
 
 bot = Bot(token=os.getenv('TELEGRAM_TOKEN'))
@@ -25,7 +25,7 @@ async def start(message: types.Message):
 
 @dp.message_handler(commands="Продал")
 async def sales_category(message: types.Message):
-    category = await db_requests(CATEGORY_PRODYCTS)
+    category = await db_requests(BotDB.get_category())
     cat_list = []
     for category_tuple in category:
         for category_buttons in category_tuple:
@@ -35,14 +35,17 @@ async def sales_category(message: types.Message):
     await message.answer("Что продал?", reply_markup=buttons)
 
 
-@dp.message_handler(commands='Напитки')
+@dp.message_handler(commands=['Напитки', 'Кофе', 'Фреш', 'Лимонад', 'Чай'])
 async def sales_prodycts(message: types.Message):
-    category = await db_requests(DRINKS)
+    products = await db_requests(
+        BotDB.get_product(
+            message.text.replace('/', '')
+            )
+        )
     response = []
-    for category_tuple in category:
-        for category_buttons in category_tuple:
-            response.append(category_buttons)
-    print(response)
+    for products_tuple in products:
+        for products_buttons in products_tuple:
+            response.append(products_buttons)
     buttons = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons.add(*response)
     await message.answer("Конкретнее", reply_markup=buttons)
--- database_queries.py
+++ database_queries.py
@@ -1,2 +1,12 @@
-CATEGORY_PRODYCTS = """SELECT name FROM category;"""
-DRINKS = """SELECT name FROM products WHERE category_id = 2;"""
+class BotDB:
+
+    def get_category():
+        req = """SELECT name FROM category;"""
+        return req
+
+    def get_product(product_name) -> str:
+        req = f'''SELECT products.name FROM products
+        JOIN category ON products.category_id = category.id
+        WHERE category.name = '{product_name}';'''
+        return req
+
